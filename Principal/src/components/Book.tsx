195834e20427d0ec0eb869b45d4e04fa3132055b
ViniciusQueiroz327 <gm.gb@outlook.com>
2024-11-15 23:34:41 -0300

    refixing routes

--- Principal/src/components/Book.tsx
+++ Principal/src/components/Book.tsx
@@ -1,12 +1,12 @@
+import { Link } from "react-router-dom"
+
 export function BookCard() {
     return (
         <>
         <div className="bookCard">
             <img src="/assets/images/livro.png" alt="Book"/>
                 <h4>As Coisas Que Você Só Vê Quando Desacelera</h4>
-            <a href="/infosLivro">
-                <button type="button">Quero esse!</button>
-            </a>
+                <button type="button"><Link to='/infosLivro'>Quero esse!</Link></button>
         </div>
         </>
     )
