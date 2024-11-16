5a721bca6d4d41f6ba22f231b91b188f2fc67190
ViniciusQueiroz327 <gm.gb@outlook.com>
2024-11-15 23:22:37 -0300

    fixing routes

--- Principal/src/App.tsx
+++ Principal/src/App.tsx
@@ -1,13 +1,32 @@
 // import './App.css'
-import { BrowserRouter } from "react-router-dom"
-import { AppRoutes } from "./routes"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+//import { AppRoutes } from "./routes"
 import '../public/assets/styles/App.css'
+
+import { Home } from './Pages/Home'; 
+import { Login } from "./Pages/Login";
+import { Register } from "./Pages/Register";
+import { InfosLivro } from "./Pages/InfosLivro";
+import { BookRegister } from "./Pages/BookRegister";
+import { BookSearch } from "./Pages/BookSearch";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+            <Route path="/home" element={<Home/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/infosLivro" element={<InfosLivro/>}/>
+            <Route path="/bookRegister" element={<BookRegister/>}/>
+            <Route path="/bookSearch" element={<BookSearch/>}/>
+      </Routes>
+    </Router>
   )
 }
 
